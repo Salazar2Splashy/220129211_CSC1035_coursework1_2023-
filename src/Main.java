1f7cb3a07f8d1d424f3bf4a53f9b2c9887ab43a5
Salazar2Splashy <c2012921@ncl.ac.uk>
2023-02-16 15:40:23 +0000

    Added main menu and ability to exit from it.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { // Creates a main menu
+        Scanner sc= new Scanner(System.in);
+        System.out.print("MAIN MENU\n" + // Displays the options for the user
+                "1. Enter Auction House data\n" +
+                "2. Enter item data\n" +
+                "3. Provide reporting statistics\n" +
+                "4. Exit\n" +
+                "Selection: ");
+        int choice= sc.nextInt(); // Checks to see what the user has inputted as their choice
+        if (choice == 4){ //If the user chooses 4, the program closes
+            System.exit(0);
+        }
+        System.out.println("Selected: " +choice);
+
     }
 }
