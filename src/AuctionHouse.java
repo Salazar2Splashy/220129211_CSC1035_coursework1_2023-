5ae7fed6a023be038cb18d10c535b6a2f77c0c1e
Salazar2Splashy <c2012921@ncl.ac.uk>
2023-02-23 18:42:20 +0000

    Fixed bug where item type was not being stored. Added javadoc.

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -67,7 +67,7 @@ public class AuctionHouse {
     public List itemsOverPrice(float minPrice) { //Used to find all items sold over a certain value
         List<Item> returnedList = new ArrayList<Item>(); //Creates an empty list
         for (Item soldItem : this.soldItems) { //Cycles through every sold item
-            if (soldItem.price >= minPrice) { //Checks if the item sold for enough money
+            if (soldItem.price > minPrice) { //Checks if the item sold for enough money
                 returnedList.add(soldItem); //Adds it to the list if it did
             }
         }
